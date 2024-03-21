fc1a5297900ba53f210462acbf9070fd17a947d0
vkurian <vkurian123@gmail.com>
2024-03-21 15:12:51 -0400

    Add script.js

--- script.js
+++ script.js
@@ -5,7 +5,30 @@ const Choice=["rock","paper","scissor"]
  function getComputerChoice(){
 
     let Computerchoice= Choice[Math.floor(Math.random() * Choice.length)];
-    console.log(Computerchoice);
+    return Computerchoice;
  }
 
  getComputerChoice()
+
+ function singleRound(playerSelection, computerSelection){
+    if (playerSelection=='rock' && computerSelection == 'scissor'
+    || playerSelection=='scissor' && computerSelection == 'paper'
+    || playerSelection=='paper' && computerSelection == 'rock'){
+        console.log(`You Win ${playerSelection} beats ${computerSelection}`)
+    }
+   else if(playerSelection=='scissor' && computerSelection == 'rock'
+   || playerSelection=='paper' && computerSelection == 'rock'
+   || playerSelection=='rock' && computerSelection == 'paper'){
+
+    console.log(`You lose ${computerSelection} beats ${playerSelection}`)
+   }
+
+   else{
+    console.log("Tie")
+   }
+
+ }
+
+const playerSelection = "rock";
+const computerSelection = getComputerChoice();
+singleRound(playerSelection, computerSelection)
